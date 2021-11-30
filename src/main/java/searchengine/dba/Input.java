2e3b0ac413a885867896abd126a1d96d847da203
emicludo <86956780+emicludo@users.noreply.github.com>
2021-11-30 10:01:47 +0100

    Input-class-created

--- src/main/java/searchengine/dba/Input.java
+++ src/main/java/searchengine/dba/Input.java
@@ -3,9 +3,11 @@ package searchengine.dba;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Input {
     private String originalInput;
+    private List<String> originalInputList;
     private List<String> inputList;
     private String finalInput;
 
@@ -13,16 +15,20 @@ public class Input {
         originalInput = input;
         split();
         trimInput();
+        lowerCase();
     }
 
     public void split() {
-        inputList = Arrays.asList(originalInput.split(" "));
+        inputList = Arrays.asList(originalInput.split("\\s+"));
+        originalInputList = Arrays.asList(originalInput.split("\\s+"));
     }
 
     public void trimInput() {
-        for (String string : inputList) {
-            string.trim();
-        }
+        inputList = inputList.stream().map(string -> string.replaceAll("[.,!?´¨^*:;}{&¤}]+", "")).collect(Collectors.toList());
+    }
+
+    public void lowerCase() {
+        inputList=inputList.stream().map(string -> string.toLowerCase()).collect(Collectors.toList());
     }
 
 }
