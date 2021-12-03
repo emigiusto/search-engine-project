c3a39b847c3c32d82ed269987a5d9de0b978d4c5
faha <faha@itu.dk>
2021-12-03 13:20:00 +0100

    CSS updates + javascript verify search result

--- web/code.js
+++ web/code.js
@@ -1,14 +1,40 @@
 /* jshint esversion: 6 */
-
-document.getElementById('searchbutton').onclick = () => {
+document.getElementById('searchbutton').onclick =  () => {
+   
     fetch("/search?q=" + document.getElementById('searchbox').value)
-    .then((response) => response.json())
-    .then((data) => {
-        document.getElementById("responsesize").innerHTML = 
-            "<p>" + data.length + " websites retrieved</p>";
-        let results = data.map((page) =>
-            `<li><a href="${page.url}">${page.title}</a></li>`)
-            .join("\n");
-        document.getElementById("urllist").innerHTML = `<ul>${results}</ul>`;
-    });
+        .then((response) => response.json())
+        .then((data) => {
+        
+            if (data.length == 1) {
+                document.getElementById("responsesize").innerHTML =
+
+                    "<p>" + data.length + " website found</p>";
+                    document.getElementById("responsesize").style =
+                    "color: darkgreen;"
+            }
+            else {
+                document.getElementById("responsesize").innerHTML =
+
+                    "<p>" + data.length + " websites found</p>";
+                    
+                    document.getElementById("responsesize").style =
+                    "color: darkgreen;"
+                    
+            }
+            let results = data.map((page) =>
+                `<li><a href="${page.url}">${page.title}</a></li>`)
+                .join("\n");
+            document.getElementById("urllist").innerHTML = `<ul>${results}</ul>`;
+
+        });
+
+    if (document.getElementById("urllist").innerHTML == "") {
+        document.getElementById("responsesize").innerHTML =
+            "<p>Sorry, no results found</p>";
+            document.getElementById("responsesize").style =
+            "color: crimson;"
+       
+           
+
+    }
 };
