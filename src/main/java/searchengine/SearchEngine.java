31e8ad1250e9e5f0a7d92b7565785f66913f145b
Klaus Lundstrøm <kllu@itu.dk>
2021-12-03 15:41:01 +0100

    Create frequencyScore for calculating score

--- src/main/java/searchengine/SearchEngine.java
+++ src/main/java/searchengine/SearchEngine.java
@@ -1,10 +1,15 @@
 package searchengine;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import searchengine.dba.Indexer;
 import searchengine.dba.WebPage;
+import searchengine.dba.Word;
 
 public class SearchEngine {
     private Indexer indexer;
@@ -21,5 +26,20 @@ public class SearchEngine {
         //Converts HashSet to ArrayList and returns result
         var result = new ArrayList<WebPage>(indexer.getWord(searchTerm).getAllWebPages());
         return result;
+    }
+    
+    public Map<WebPage, Double> frequencyScore(String searchTerm) {
+        Word wordSearched = indexer.getWord(searchTerm);
+
+        Set<WebPage> allWebPages = wordSearched.getAllWebPages();
+        double totalFrequency = wordSearched.getTotalFrequency();
+        
+        Map <WebPage, Double> frequencyHits = new HashMap<>();
+        for (WebPage webPage : allWebPages) {
+            double frequency = wordSearched.getWebPageFrequency(webPage);
+            double idfScore = frequency / totalFrequency;
+            frequencyHits.put(webPage, idfScore);
+        }
+        return frequencyHits;
     }
 }
