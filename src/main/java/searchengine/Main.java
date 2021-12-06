b06a6f24b6b57f9da60c032f6cbf879441f9bba7
emicludo <86956780+emicludo@users.noreply.github.com>
2021-12-06 21:34:17 +0100

    clean-Main

--- src/main/java/searchengine/Main.java
+++ src/main/java/searchengine/Main.java
@@ -4,15 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import searchengine.dba.Input;
-
 public class Main {
     static final int PORT = 8080;
     public static void main(final String... args) throws IOException {
-        //var filename = Files.readString(Paths.get("config.txt")).strip();
-        //var newServer = new WebServer(PORT, filename);
-
-        var input = new Input(",.fIRst    ..seCoNd,..");
-        System.out.println(input);
+        var filename = Files.readString(Paths.get("config.txt")).strip();
+        new WebServer(PORT, filename);
       }
 }
