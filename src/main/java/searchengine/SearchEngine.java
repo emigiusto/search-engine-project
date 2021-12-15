eb81ebb8e61975b89bb6728483682493a8a53f09
Klaus Lundstrøm <kllu@itu.dk>
2021-12-15 11:55:46 +0100

    Method for mapping webpages and frequencyscores

--- src/main/java/searchengine/SearchEngine.java
+++ src/main/java/searchengine/SearchEngine.java
@@ -1,8 +1,10 @@
 package searchengine;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +18,7 @@ public class SearchEngine {
     private Indexer indexer;
     private String searchInput;
     private List<List<String>> splittedInput = new ArrayList<>();
+    private Map<WebPage, Double> resultMap = new HashMap<>();
 
     public SearchEngine(String filename) {
         try {
@@ -28,6 +31,7 @@ public class SearchEngine {
     public List<WebPage> search(String searchTerm) {
         searchInput = searchTerm;
         splittingInput();
+        Set<WebPage>setOfWebpages = gatherWebpages();
         var result = new ArrayList<WebPage>(indexer.getWord(searchTerm).getAllWebPages());
         return result;
     }
@@ -40,12 +44,34 @@ public class SearchEngine {
         }
     }
 
-    public void name() {
+    public Set<WebPage> gatherWebpages() {
+        Set<WebPage> setOfWebpages = new HashSet<>();
+
         for (List<String> listJoinedByAnd : splittedInput) {
-            
+            //("Danish university or something else OR cookies oR mate");
+            HashMap<WebPage,Double> andHashmap = getScoreWithANDlogic(listJoinedByAnd);
         }
+        return setOfWebpages;
     }
     
+    public HashMap<WebPage,Double> getScoreWithANDlogic(List<String> listWithANDogic){
+        HashMap<WebPage,Double> mapOfWebPages = new HashMap<>();
+        for (String term : listWithANDogic) {
+            Word wordSearched = indexer.getWord(term);
+            //("Danish university or something else OR cookies oR mate");
+            Set<WebPage> allWebPages = wordSearched.getAllWebPages();
+            for (WebPage webPage : allWebPages) {
+                if (mapOfWebPages.containsKey(webPage)) {
+                    Double totalScore = mapOfWebPages.get(webPage)+getPageScore(wordSearched,webPage);
+                    mapOfWebPages.put(webPage,totalScore);
+                } else {
+                    mapOfWebPages.put(webPage, getPageScore(wordSearched,webPage));
+                }
+            }
+        }
+        return mapOfWebPages;
+    }
+
     public Map<WebPage, Double> frequencyScore(String searchTerm) {
         Word wordSearched = indexer.getWord(searchTerm);
 
@@ -54,7 +80,7 @@ public class SearchEngine {
         Map <WebPage, Double> frequencyHits = new HashMap<>();
         for (WebPage webPage : allWebPages) {
             var idfScore = getPageScore(wordSearched, webPage); 
-            frequencyHits.put(webPage, idfScore);
+            resultMap.put(webPage, idfScore);
         }
         return frequencyHits;
     }
