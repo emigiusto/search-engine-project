d36a262a853dd9f5d16cf5ed8f616979403f29d0
emicludo <86956780+emicludo@users.noreply.github.com>
2021-12-19 12:40:12 +0100

    add-mockDBA and SearchEngineTest

--- src/test/java/searchengine/SearchEngineTest.java
+++ src/test/java/searchengine/SearchEngineTest.java
@@ -17,13 +17,12 @@ import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(Lifecycle.PER_CLASS)
 class SearchEngineTest {
-    String filename;
-    String searchInput1, searchInput2;
-    SearchEngine searchEngine;
+    SearchEngine searchEngine,searchEngineMock;
     
     @BeforeEach                                         
     void setUp() {
         searchEngine = new SearchEngine("data/enwiki-tiny.txt");
+        searchEngineMock = new SearchEngine("data/enwiki-mockdba.txt");
     }
     
     @Test
@@ -55,7 +54,7 @@ class SearchEngineTest {
     @DisplayName("getPageScore should calculate the score of the input")
     void testGetPageScore() {
         WebPage testWebPage = new WebPage("Danish universities", "www.wikipedia.com");
-        WebPage testWebPage2 = new WebPage("Danish universities", "www.wikipedia.com");
+        WebPage testWebPage2 = new WebPage("Danish universities2", "www.wikipedia.com2");
         Word testWord = new Word("danish", testWebPage);
         //First Case
         testWord.addOcurrence(testWebPage2);
@@ -68,38 +67,98 @@ class SearchEngineTest {
     }
 
     @Test
-    @DisplayName("getScoreMapWithANDlogic should calculate score of input without 'or' operators and put in List")
-    void testgetScoreMapWithANDlogic() {
-    /*    var searchengine1 = new SearchEngine("data/enwiki-tiny.txt");
+    @DisplayName("createANDLogicMap should return a HashMap with all webpages related to each of the input strings and their corresponding score using AND logic")
+    void testCreateANDLogicMap() {
+        List<String> testList = new ArrayList<String>();
+        testList.add("colombia");
+        testList.add("iron");
 
-
-        HashMap<WebPage,Double> actual = new HashMap<>();
-        
-        WebPage testWebPage = new WebPage("Danish universities", "www.wikipedia.com");
-        WebPage testWebPage2 = new WebPage("Danish universities", "www.wikipedia.com");
-        Word testWord = new Word("danish", testWebPage);
-        testWord.addOcurrence(testWebPage2);
-        
-        searchengine1.getPageScore(testWord, testWebPage);
-        assertEquals(searchengine1.getPageScore(testWord, testWebPage), 0.5);
-
-        testWord.addOcurrence(testWebPage2);
-        testWord.addOcurrence(testWebPage2);
-        assertEquals(searchengine1.getPageScore(testWord, testWebPage), 0.25);*/
+        //Case Colombia Iron -> mockdba.txt database
+        Word colombiaWord = searchEngineMock.getIndexer().getWord("colombia");
+        List<WebPage> webPageList = new ArrayList<WebPage>();
+        webPageList.addAll(colombiaWord.getAllWebPages());
+        WebPage colombiaPage = webPageList.get(0);
+        WebPage ironPage = webPageList.get(1);
+        assertEquals(searchEngineMock.createANDLogicMap(testList).get(colombiaPage), 0.75); //0.75 from Colombia Word + 0 from Iron Word
+        assertEquals(searchEngineMock.createANDLogicMap(testList).get(ironPage), 1.25); //1 from Iron Word + 0.25 from Colombia Word
     }
 
     @Test
-    @DisplayName("getScoreMapWithORlogic should calculate score of input without 'or' operators and put in Map")
-    void testgetScoreMapWithORlogic() {
-   
-    }
- 
-    
-    @Test
-    @DisplayName("gatherWebpages should gather webpages and scores corresponding to the input ")
-    void testgatherWebpages() {
+    @DisplayName("mergeORLogicMaps should return a single HashMap from one or multiple HashMaps as input, keeping the maximun value when finding duplicate entries")
+    void testMergeORLogicMaps() {
+        WebPage testWebPage1 = new WebPage("Title1", "URL1");
+        WebPage testWebPage2 = new WebPage("Title2", "URL2");
+        var hashmap1 = new HashMap<WebPage,Double>();
+            hashmap1.put(testWebPage1, (double) 8);
+            hashmap1.put(testWebPage2, (double) 1);
+        var hashmap2 = new HashMap<WebPage,Double>();
+            hashmap2.put(testWebPage1, (double) 1);
+            hashmap2.put(testWebPage2, (double) 10);
+        //Create Input List
+        var inputHashmapList = new ArrayList<HashMap<WebPage,Double>>();
+            inputHashmapList.add(hashmap1);
+            inputHashmapList.add(hashmap2);
+        //Create expected hashmap with Maximun values
+        var expectedHashmap = new HashMap<WebPage,Double>();
+            expectedHashmap.put(testWebPage1,(double) 8);
+            expectedHashmap.put(testWebPage2, (double) 10);
 
+        assertEquals(searchEngine.mergeORLogicMaps(inputHashmapList),expectedHashmap);
     }
-    
+
+    @Test
+    @DisplayName("cleanWord should return a stemmed lowercase version of the input word with all surrounding signs removed")
+    void testCleanWord() {
+        String input1 = ",,between!";
+        String input2 = "beliEVed";
+        String input3 = "peacefuL";
+        String input4 = "!Misleading";
+        assertEquals(searchEngine.cleanWord(input1),"between");
+        assertEquals(searchEngine.cleanWord(input2),"believ");
+        assertEquals(searchEngine.cleanWord(input3),"peac");
+        assertEquals(searchEngine.cleanWord(input4),"mislead");
+    }
+
+    @Test
+    @DisplayName("compileHashMaps should the final hashmap with AND and OR logic operated in the values")
+    void testCompileHashMaps() {
+        //searchInput is "colombia notarealword or iron"
+        List<String> testList1 = new ArrayList<String>();
+            testList1.add("colombia");
+            testList1.add("notarealword");
+        List<String> testList2 = new ArrayList<String>();
+            testList2.add("iron");
+        List<List<String>> testListListInput = new ArrayList<List<String>>();
+            testListListInput.add(testList1);
+            testListListInput.add(testList2);
+        
+        //Case Colombia Iron -> mockdba.txt database
+        Word colombiaWord = searchEngineMock.getIndexer().getWord("colombia");
+        List<WebPage> webPageList = new ArrayList<WebPage>();
+        webPageList.addAll(colombiaWord.getAllWebPages());
+        //Selects the only 2 webpages present in the database
+        WebPage colombiaPage = webPageList.get(0);
+        WebPage ironPage = webPageList.get(1);
+        
+        assertEquals(searchEngineMock.compileHashMaps(testListListInput).get(colombiaPage), 1.25);
+        assertEquals(searchEngineMock.compileHashMaps(testListListInput).get(ironPage), 1);
+    }
+
+    @Test
+    @DisplayName("search should return a list of two pages 1)Colombia 2)Iron when searching for 'colombia notarealword or iron'")
+    void testSearch() {
+        //Case Colombia Iron -> mockdba.txt database
+        Word colombiaWord = searchEngineMock.getIndexer().getWord("colombia");
+        List<WebPage> webPageList = new ArrayList<WebPage>();
+        webPageList.addAll(colombiaWord.getAllWebPages());
+        //Selects the only 2 webpages present in the database
+        WebPage colombiaPage = webPageList.get(0);
+        WebPage ironPage = webPageList.get(1);
+
+        //Colombia WebPage should be second (Score = 1.25)
+        //Iron WebPage should be second (Score = 1)
+        assertEquals(searchEngineMock.search("colombia%20notarealword or iron").get(0), colombiaPage);
+        assertEquals(searchEngineMock.search("colombia%20notarealword or iron").get(1), ironPage);
+    }
 }
 
